d35ac835c096d040d86622cd3a8b4f1b0a982e6c
Ivan ROGER <ivan.bob.emile@gmail.com>
2016-10-17 15:42:56 +0000

    Character details + Markdown in descriptions.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# --- --- IMPORTS --- ---
+
 import ConfigParser
 import json
-from flask import Flask, render_template, flash
+import markdown
+from os import listdir
+from os.path import basename
+from flask import Flask, render_template, flash, redirect, url_for
+
+# --- --- GLOBAL VARS --- ---
+
 app = Flask(__name__)
 app.secret_key="Ic&Ts3IuNS*uAQbc#nur2UUAAme$8xD|"
-app_config = { # Will be loaded in 'init()'
-	'graphic': {
-		'default_universe_pic': "img/default_universe_pic.jpg"
-	},
-	'data': {}
-}
+app_config = { 'graphic': {}, 'data': {} } # Will be loaded in 'init()'
 app_nav = [
 	{'name': "Home", 'path': "/"},
 	{'name': "About", 'path': "/about"},
 	{'name': "Universes", 'path': "/universes"},
 	{'name': "Characters", 'path': "/characters"}
 ]
+data_cache = {
+	'universes': {}, 'universe_tags': [],
+	'characters': {}, 'character_tags': [],
+}
 
 # --- --- ROUTES --- ---
 
@@ -26,30 +33,87 @@ def route_root():
 
 @app.route('/about')
 def route_about():
+	loadUniverseList()
+	flash("Reloaded "+str(len(data_cache['universes']))+" universes.")
+	loadCharacterList()
+	flash("Reloaded "+str(len(data_cache['characters']))+" characters.")
 	data = {'config': app_config, 'nav': app_nav, 'active': "/about"}
 	return render_template('about.html', data=data)
 
+# Universes
 @app.route('/universes')
 def route_universes():
-	index_file = open(app_config['data']['data_folder']+"/universes/"+app_config['data']['index_file'], "r")
-	univ_list = json.load(index_file)
-	data = {'config': app_config, 'nav': app_nav, 'active': "/universes", 'list': univ_list}
+	data = {'config': app_config, 'nav': app_nav, 'active': "/universes", 'list': getUniverseList()}
 	return render_template('universes.html', data=data)
 
 @app.route('/universes/<univID>')
 def route_universe(univID):
-	data = {'config': app_config, 'nav': app_nav, 'active': "/universes", 'univ': {}}
+	info_file = open(app_config['data']['data_folder']+"/universes/"+univID+".json", "r") # TODO: escape univID
+	univ_info = parseDown(json.load(info_file))
+	info_file.close()
+	data = {'config': app_config, 'nav': app_nav, 'active': "/universes", 'univ': univ_info}
 	return render_template('universe-details.html', data=data)
 
+# Charcaters
 @app.route('/characters')
 def route_characters():
-	data = {'config': app_config, 'nav': app_nav, 'active': "/characters", 'list': []}
+	data = {'config': app_config, 'nav': app_nav, 'active': "/characters", 'list': getCharacterList()}
 	return render_template('characters.html', data=data)
 
 @app.route('/characters/<charID>')
 def route_character(charID):
-	data = {'config': app_config, 'nav': app_nav, 'active': "/characters", 'char': {}}
+	info_file = open(app_config['data']['data_folder']+"/characters/"+charID+".json", "r") # TODO: escape charID
+	char_info = parseDown(json.load(info_file))
+	info_file.close()
+	data = {'config': app_config, 'nav': app_nav, 'active': "/characters", 'char': char_info}
 	return render_template('character-details.html', data=data)
+
+# --- --- DATA PROVIDERS --- ---
+def parseDown(item):
+	item['short_desc'] = markdown.markdown(item['short_desc'])
+	item['full_desc'] = markdown.markdown(item['full_desc'])
+	return item
+
+def getUniverseList():
+	return data_cache['universes'].values()
+
+def loadUniverseList():
+	folderPath = app_config['data']['data_folder']+"/universes/"
+	print "Loading universes ( from:", folderPath, ")"
+	# Reset
+	data_cache['universe_tags'] = {}
+	# Walking on each file
+	files = listdir(folderPath)
+	for info_file in files:
+		print "\tFile:", info_file
+		# Loading JSON Object from file
+		filePtr = open(folderPath+info_file)
+		jsonObj = json.load(filePtr)
+		filePtr.close()
+		# Caching data
+		data_cache['universes'][jsonObj['id']] = jsonObj
+		# Update tag list
+		for t in jsonObj['tags']:
+			if t in data_cache['universe_tags']:
+				data_cache['universe_tags'][t].append(jsonObj['id'])
+			else:
+				data_cache['universe_tags'][t] = [jsonObj['id']]
+	print "DONE ! Loaded", len(data_cache['universes']), "files."
+
+def getCharacterList():
+	return data_cache['characters']
+
+def loadCharacterList():
+	folderPath = app_config['data']['data_folder']+"/characters/"
+	print "Loading characters ( from:", folderPath, ")"
+	files = listdir(folderPath)
+	data_cache['characters'] = []
+	for info_file in files:
+		print "\tLoading file:", info_file
+		filePtr = open(folderPath+info_file)
+		data_cache['characters'].append(json.load(filePtr))
+		filePtr.close()
+	print "DONE ! Loaded", len(data_cache['characters']), "files."
 
 # --- --- ERRORS --- ---
 
@@ -69,10 +133,12 @@ def init(app):
 		app_config['app_name'] = config.get("app", "name")
 		app_config['app_author'] = config.get("app", "author")
 		app_config['app_contact'] = config.get("app", "contact")
+		# Graphic
+		app_config['graphic']['default_universe_pic'] = config.get("graphic", "default_universe_pic")
+		app_config['graphic']['default_character_pic'] = config.get("graphic", "default_character_pic")
 		# Data
 		app_config['data'] = {}
 		app_config['data']['data_folder'] = config.get("data", "ressource_folder")
-		app_config['data']['index_file'] = config.get("data", "index_file")
 		# Config
 		app.config['DEBUG'] = config.get("config", "debug")
 		app.config['ip_address'] = config.get("config", "ip_address")
@@ -82,6 +148,9 @@ def init(app):
 	except IOError as e:
 		print "ERROR: Could not read configs from", config_location
 		print "\t>>", e
+	# loading cached data
+	loadUniverseList()
+	loadCharacterList()
 
 if __name__ == '__main__':
     init(app)
